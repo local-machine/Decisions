3bbef9c0e916b0cec84b3b46b18a2e0e208ca813
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-07-18 14:11:39 -0700

    Created project to practice variables

--- project.cs
+++ project.cs
@@ -1,11 +1,20 @@
 using System;
 
-class //insert class name 
+namespace Variables 
 {
-    static void Main()
+    class Program 
     {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Enter your first name: ");
+            string userFirstName = Console.ReadLine();
 
+            Console.WriteLine("Enter your last name: ");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userFirstName + " " + userLastName);
+            Console.ReadLine();
+        }
     }
-
-
 }
